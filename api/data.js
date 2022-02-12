9b642af3b86b670372d6f82c91a34392bdd62d32
David Hevesi <davidhevesi2008@gmail.com>
2022-02-12 14:48:51 -0500

    add mock data

---
+++ api/data.js
@@ -0,0 +1,29 @@
+export const resources = [
+  {
+    id: '1',
+    title: 'Learn new pattern',
+    description: 'Learning all things javascript',
+    link: 'https://link.com',
+    priority: '3',
+    timeToFinish: 120,
+    active: false,
+  },
+  {
+    id: '2',
+    title: 'Learn new pattern',
+    description: 'Learning new',
+    link: 'https://link.com',
+    priority: '1',
+    timeToFinish: 120,
+    active: false,
+  },
+  {
+    id: '3',
+    title: 'Learn new pattern',
+    description: 'Learning new',
+    link: 'https://link.com',
+    priority: '2',
+    timeToFinish: 120,
+    active: false,
+  },
+];
